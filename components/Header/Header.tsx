b895d4f05a764bd88cc267b26b82cf1110c2907a
Wuji <121530145+WujiShelby@users.noreply.github.com>
2024-12-12 11:23:32 +0700

    update logo header
    create header about logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Image from 'next/image'
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      {/* Logo */}
+      <div>
+        <Image
+          src="/logo-axion-trade.png"
+          width={300}
+          height={300}
+          alt="Picture of the author"
+          className="bg-black"
+        />
+      </div>
+      {/* Menu */}
+      <nav>
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+
+      </div>
+    </header>
+  );
 };
 
 export default Header;
