17abb11f74952d6cea0dc6593ed41098227a1564
Wuji <121530145+WujiShelby@users.noreply.github.com>
2024-12-12 15:25:46 +0700

    Update Header
    Page Title Not yet

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,30 +1,51 @@
 import React from "react";
 import Image from 'next/image'
+import Link from 'next/link'
+import { FaUserCircle } from "react-icons/fa";
+import { GrLanguage } from "react-icons/gr";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header>
+    <header className="grid grid-cols-3 gap-4 items-center p-4 pb-96 bg-[url('../assets/images/bg-axion-trade.png')] bg-cover bg-center">
       {/* Logo */}
-      <div>
-        <Image
-          src="/logo-axion-trade.png"
-          width={300}
-          height={300}
-          alt="Picture of the author"
-          className="bg-black"
-        />
-      </div>
-      {/* Menu */}
-      <nav>
-        <ul>
-          <li>
+      <div className="flex justify-center">
+        <Link href="/">
+          <Image
+            src="/logo-axion-trade.png"
+            width={150}
+            height={150}
+            alt="Axion Trade Logo"
+          />
+        </Link>
 
-          </li>
-        </ul>
+      </div>
+
+      {/* Menu */}
+      <nav className="flex justify-center space-x-10">
+        <Link href="/" className="text-white hover:text-white ">
+          Home
+        </Link>
+        <Link href="/markets" className="text-white hover:text-white">
+          Markets
+        </Link>
+        <Link href="/accounts" className="text-white hover:text-white">
+          Accounts
+        </Link>
+        <Link href="/platforms" className="text-white hover:text-white">
+          Platforms
+        </Link>
       </nav>
 
-      <div>
+      {/* Other */}
 
+      <div className="flex justify-center items-center space-x-8">
+        <div className="flex space-x-2">
+          <FaUserCircle size={25} color="white" />
+          <span className="text-white hover:text-white">Client Portal</span>
+        </div>
+        <GrLanguage size={25} color="white" />
+        <HiMenuAlt3 size={35} color="white" />
       </div>
     </header>
   );
