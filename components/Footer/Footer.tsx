100d56555a465fd578881c31768df923404fceb2
AndyPovv <povbros105@gmail.com>
2024-12-12 11:33:18 +0700

    update footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,35 @@
 import React from "react";
+import Image from "next/image";
+import SubTitle from "@/app/shared/sub-title";
 
 const Footer = () => {
- return (
-  <footer>
-    <div className="bg-black">
-      
-      hq
-  
+  return (
+    <footer className="flex grid-cols-4 gap-5">
+      <div className="bg-black">
+        <div>
+        <Image
+          src="/logo-axion-trade.png"
+          width={300}
+          height={500}
+          alt="Picture of the author"
+        />
+        </div>
+        <div>
 
-    </div>
- </footer>
- );
-
-  
+        </div>
+       
+      </div>
+      <div>
+        <SubTitle subTitle="MARKET"/>
+      </div>
+      <div>
+      <SubTitle subTitle="COMPANY"/>
+      </div>
+      <div>
+      <SubTitle subTitle="GET IN TOUCH"/>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
