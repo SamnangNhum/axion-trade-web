cd0efa2e3134791a537901d56435f75b44908e05
Wuji <121530145+WujiShelby@users.noreply.github.com>
2024-12-16 09:51:08 +0700

    Edit Package Json

--- app/shared/title.tsx
+++ app/shared/title.tsx
@@ -1,6 +1,10 @@
 export default function Title({title,otherClass}:{title:string,otherClass?:string}){
     return(
+<<<<<<< Updated upstream
         <div className={`text-5xl font-bold text-black ${otherClass}`}>
+=======
+        <div className="text-5xl font-bold text-white">
+>>>>>>> Stashed changes
             {title}
         </div>
     );
